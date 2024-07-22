26243cb6e4aa9cd08d8f6bc0377aa3aae34850a4
HuiSeopKwak <kh06033@naver.com>
2024-07-22 22:33:55 +0900

    0722 algo update

--- src/boj1749.java
+++ src/boj1749.java
@@ -6,8 +6,11 @@ import java.util.StringTokenizer;
  * 백준 11660 구간 함 구하기
 
 */
-public class boj1749 {
 
+public class boj1749 {
+//public class Main {
+	static long maxSum = Integer.MIN_VALUE;	// 0으로 하면 안됌...
+	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
@@ -27,14 +30,29 @@ public class boj1749 {
 		for (int i = 1; i < n + 1; i++) {
 			for (int j = 1; j < m + 1; j++) {
 				Sboard[i][j] = Sboard[i][j-1] + Sboard[i-1][j] - Sboard[i-1][j-1] + board[i][j];
+//				System.out.print(Sboard[i][j] + " ");
+			}
+//			System.out.println();
+		}
+		// 부분 수열 브루투 포스로 최대값 계산
+		for (int x = 1; x < n + 1; x++) {
+			for (int y = 1; y < m + 1; y++) {
+				for (int i = x; i < n + 1; i++) {
+					for (int j = y; j < m + 1; j++) {
+						maxCount(x, y, i, j, Sboard);
+					}
+				}
 			}
 		}
-		// 부분 배열 최대합 구하기
-		for (int i = 1; i < n + 1; i++) {
-			for (int j = 1; j < m + 1; j++) {
-				Sboard[i][j] = Sboard[i][j-1] + Sboard[i-1][j] - Sboard[i-1][j-1] + board[i][j];
-			}
+		System.out.println(maxSum);
+	}
+	
+	// 부분 배열 최대값 찾는 함수
+	public static void maxCount(int x, int y, int i, int j, int [][] Sboard) {
+		long temp = Sboard[i][j] - Sboard[x-1][j] - Sboard[i][y-1] + Sboard[x-1][y-1];
+		if (temp > maxSum) {
+//			System.out.printf("x = %d, y = %d, i = %d, j = %d\nmaxsum = %d\nSboard[i][j] = %d - Sboard[x-1][j] = %d - Sboard[i][y-1] = %d + Sboard[x-1][y-1] = %d\n", x, y, i, j, temp, Sboard[i][j], Sboard[x-1][j], Sboard[i][y-1], Sboard[x-1][y-1]);
+			maxSum = temp;
 		}
-		
 	}
 }
--- src/boj20438.java
+++ src/boj20438.java
@@ -3,35 +3,77 @@
  * 학생 번호는 3 ~ n+2 까지
 
 */
-import java.util.Scanner;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class boj20438 {
+//public class Main {
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		// 1번째줄 입력
-		int n = sc.nextInt();	// 저체 학생 수
-		int k = sc.nextInt();	// 조는 학생 수
-		int q = sc.nextInt();	// 출석 코드 보낼 학생 수
-		int m = sc.nextInt();	// 주어질 구간의 수
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
 		
-		int[] k_stu = new int[k];	// 조는 학생들의 입장 번호 배열
-		int[] q_stu = new int[q];	// 출석 코드 보낼 학생들의 입장 번호 배열
+		// 1번째줄 입력
+		int n = Integer.parseInt(st.nextToken());	// 저체 학생 수
+		int k = Integer.parseInt(st.nextToken());	// 조는 학생 수
+		int q = Integer.parseInt(st.nextToken());	// 출석 코드 보낼 학생 수
+		int m = Integer.parseInt(st.nextToken());	// 주어질 구간의 수
+		
+		int[] stuArr = new int[n+3];	// 출석 체크용 학생 배열
+		int[] k_stu = new int[n+3];		// 조는 학생들의 입장 번호 배열
+		int[] Sum = new int[n+3];		// 누적합 배열
+		int temp = 0;					// 조는 학생번호 임시 저장용
+		int code = 0;					// 출석 코드 받는 학생 임시 저장용
 		
 		// 2번째줄 조는 학생 입장 번호
+		st = new StringTokenizer(br.readLine());
 		for (int i = 0; i < k; i++) {
-			k_stu[i] = sc.nextInt();
+			 temp = Integer.parseInt(st.nextToken());
+			 k_stu[temp] = 1;
 		}
-		// 3번째줄 출석 코드 보낼 학생 번호
+		
+		// 3번째줄 출석 코드 보낼 학생 번호 받아서 출석 하는 인원 파악
+		st = new StringTokenizer(br.readLine());
 		for (int i = 0; i < q; i++) {
-			q_stu[i] = sc.nextInt();
+			code = Integer.parseInt(st.nextToken());
+			int value = code;			// 배수 호출용
+			while (code < n+3) {
+				if (k_stu[code] == 0) {	// 불렸고, 깨있으면 출석 인정해줌
+					stuArr[code] = 1;	// stuArr배열에서 자기번호가 1이면 출석인정, 0은 출석 인정 X
+					code += value;
+				}
+				else
+					code += value;
+			}
 		}
-		// 4번째줄 구간 간격
-		int s = sc.nextInt();
-		int e = sc.nextInt();
 		
+		// 번호 순 출석안된 누적 합 배열 구하기
+		for (int i = 3; i < n+3; i++) {
+			if (stuArr[i] == 0) {
+				Sum[i] = Sum[i-1] + 1;
+			}
+			else
+				Sum[i] = Sum[i-1];
+		}
 		
-
+//		for (int i = 3; i < n+3; i++) {
+//			System.out.println(i + " " + stuArr[i] + " " + Sum[i]);
+//		}
+		
+		// 4번째줄 구간 간격 받으면서 누적합 배열 이용해서 프린트
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < m; i++) {
+			st = new StringTokenizer(br.readLine());
+			int s = Integer.parseInt(st.nextToken());
+			int e = Integer.parseInt(st.nextToken());
+			sb.append(Sum[e] - Sum[s-1]).append("\n");
+		}
+		System.out.println(sb);
 	}
 
 }

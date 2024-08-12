f2cb2f2d6bb4162dd11e92e2fc15b0fa6662cd86
HuiSeopKwak <kh06033@naver.com>
2024-08-12 21:11:12 +0900

    desktop update

--- src/algo/study2/week1/boj15666.java
+++ src/algo/study2/week1/boj15666.java
@@ -1,7 +1,9 @@
 package algo.study2.week1;
 
 import java.util.Arrays;
+import java.util.LinkedHashSet;
 import java.util.Scanner;
+import java.util.Set;
 import java.util.Stack;
 
 public class boj15666 {
@@ -9,33 +11,38 @@ public class boj15666 {
 	static int N, M;
 	static Stack<Integer> que = new Stack<Integer>();
 	static int arr[];
+	static Set<String> set = new LinkedHashSet<>();
 	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		N = sc.nextInt();
 		M = sc.nextInt();
-		arr = new int[N+1];
+		arr = new int[N];
 		for (int i = 0; i < N; i++) {
 			arr[i] = sc.nextInt();
 		}
 		Arrays.sort(arr);
-		
 		recur(0);
+		
+		for (String str : set) {
+			System.out.println(str);
+		}
 	}
 	
 	static void recur(int idx) {
 		if (idx == M) {
+			String str = "";
 			for (int num : que) {
-				System.out.print(num + " ");
+				str = str + num + " ";
 			}
-			System.out.println();
+			set.add(str);
 			return;
 		}
-		for (int i = 1; i < N+1; i++) {
-			if (!que.isEmpty() && i <= que.peek())
+		for (int i = 0; i < N; i++) {
+			if (!que.isEmpty() && arr[i] < que.peek())
 				continue;
 			
-			que.add(i);
+			que.add(arr[i]);
 			recur(idx+1);
 			que.pop();
 		}

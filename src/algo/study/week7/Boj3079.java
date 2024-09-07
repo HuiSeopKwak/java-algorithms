c7ca1817fcfa9ec0961372b63b57f1e1a36693b3
HuiSeopKwak <kh06033@naver.com>
2024-09-07 19:12:42 +0900

    0907 SAT algo update

---
+++ src/algo/study/week7/Boj3079.java
@@ -0,0 +1,23 @@
+package algo.study.week7;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+/**
+ * 백준 22871 징검다리 건너기 메모리 : KB 시간 : ms
+ */
+
+public class Boj3079 {
+	static int N, M;
+	static int T[];
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+
+	}
+
+}

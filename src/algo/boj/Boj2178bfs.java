71572f65a2cfb8450f2e2fc307df7949016e98d7
곽희섭 <kh06033@naver.com>
2025-02-26 17:45:34 +0900

    solve: 2178 미로탐색

--- src/algo/boj/Boj2178bfs.java
+++ src/algo/boj/Boj2178bfs.java
@@ -7,14 +7,12 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
-
-// dfs 시간 초과 코드
 public class Boj2178bfs {
     static int N, M;
-    static int minMove = Integer.MAX_VALUE;
     static int map[][];
     static int dy[] = {1, 0, -1, 0};
     static int dx[] = {0, 1, 0, -1};
+    static int dist[][];
     static boolean visited[][];
 
     public static void main(String[] args) throws IOException {
@@ -24,6 +22,7 @@ public class Boj2178bfs {
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
         map = new int[N+1][M+1];
+        dist = new int[N+1][M+1];
         visited = new boolean[N+1][M+1];
 
         for (int i = 1; i <= N; i++) {
@@ -36,12 +35,27 @@ public class Boj2178bfs {
 
         // BFS
         bfs(1, 1);
+
+        System.out.println(dist[N][M]);
     }
 
     public static void bfs(int y, int x) {
         Queue<int[]> que = new LinkedList<>();
         que.add(new int[]{y, x});
+        dist[y][x] = 1;
 
-
+        // que가 비지 않을때 까지 반복
+        while(!que.isEmpty()) {
+            int[] temp = que.poll();
+            for (int d = 0; d < 4; d++) {
+                int ny = temp[0] + dy[d];
+                int nx = temp[1] + dx[d];
+                if (ny > 0 && nx > 0 && ny <= N && nx <= M && !visited[ny][nx] && map[ny][nx] == 1) {
+                    que.add(new int[]{ny, nx});
+                    visited[ny][nx] = true;
+                    dist[ny][nx] = dist[temp[0]][temp[1]] + 1;
+                }
+            }
+        }
     }
 }

e5cdc322df30e85176121f1444eb85db63ba6eb2
곽희섭 <kh06033@naver.com>
2025-02-27 14:03:32 +0900

    solve: 2667 단지번호붙이기

--- src/algo/boj/Boj2667.java
+++ src/algo/boj/Boj2667.java
@@ -3,15 +3,17 @@ package algo.boj;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class Boj2667 {
     static int size;
     static int map[][];
     static List<Integer> result = new ArrayList<>();
-    static int house;
+    static int totalHouses = 1;
+    static int houseCounts = 0;
     static boolean visited[][];
+    static int dy[] = {1, 0, -1, 0};
+    static int dx[] = {0, 1, 0, -1};
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -29,6 +31,40 @@ public class Boj2667 {
         // 입력 끝
 
         // bfs로 0, 0의 자리부터 탐색
-        bfs(0, 0);
+        for (int y = 0; y < size; y++) {
+            for (int x = 0; x < size; x++) {
+                // 집이 있어야하고, visited 된곳은 방문 X
+                if (map[y][x] == 1 && !visited[y][x]) {
+                    bfs(y, x);
+                    totalHouses++;
+                }
+            }
+        }
+
+        System.out.println(totalHouses - 1);
+        Collections.sort(result);
+        for (int i : result)
+            System.out.println(i);
+    }
+
+    public static void bfs(int y, int x) {
+        Queue<int []> que = new LinkedList<>();
+        que.add(new int[] {y, x});
+        visited[y][x] = true;
+        houseCounts = 1;
+
+        while(!que.isEmpty()) {
+            int[] temp = que.poll();
+            for (int d = 0; d < 4; d++) {
+                int ny = temp[0] + dy[d];
+                int nx = temp[1] + dx[d];
+                if (ny >= 0 && nx >= 0 && ny < size && nx < size && map[ny][nx] == 1 && !visited[ny][nx]) {
+                    visited[ny][nx] = true;
+                    houseCounts++;
+                    que.add(new int[] {ny, nx});
+                }
+            }
+        }
+        result.add(houseCounts);
     }
 }

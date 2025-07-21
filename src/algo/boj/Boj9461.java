b7889bbe39a8e96f8eeb27194f193a9b16062b1e
HuiSeopKwak <kh06033@naver.com>
2025-07-21 14:41:33 +0900

    solve: 파도반 수열

---
+++ src/algo/boj/Boj9461.java
@@ -0,0 +1,26 @@
+package algo.boj;
+/**
+ * 백준 9461 파도반 수열
+ */
+
+import java.util.Scanner;
+
+public class Boj9461 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        long[] dp = new long[101];
+        dp[1] = 1;
+        dp[2] = 1;
+        dp[3] = 1;
+
+        for (int i = 4; i <= 100; i++) {
+            dp[i] = dp[i - 2] + dp[i - 3];
+        }
+
+        for (int tc = 0; tc < T; tc++) {
+            int n = sc.nextInt();
+            System.out.println(dp[n]);
+        }
+    }
+}
